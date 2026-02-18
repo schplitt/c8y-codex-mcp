e99dff22b2b4b105ba84407d81b77d0e82dbee18
schplitt <schplitt.contact@gmail.com>
2026-02-18 10:06:05 +0100

    up

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,7 @@ import { wasm } from '@rollup/plugin-wasm';
 import { defineNitroConfig } from "nitropack/config"
 // https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: "2026-01-01",
+  compatibilityDate: "latest",
   srcDir: "server",
   preset: "cloudflare_module",
   imports: false,
@@ -25,6 +25,10 @@ export default defineNitroConfig({
           binding: "CACHE",
           id: "4c5baf90254446f08fe2f88c15a00a76"
         }
+      ],
+      compatibility_flags: [
+        "nodejs_compat",
+        "nodejs_compat_v2"
       ]
     }
   }
