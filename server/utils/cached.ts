7991ecd53f51c79ceba555465c57e8b9eca446d4
schplitt <schplitt.contact@gmail.com>
2026-02-18 09:52:16 +0100

    feat: add maxAge configuration to useCodexContext for caching

--- server/utils/cached.ts
+++ server/utils/cached.ts
@@ -4,4 +4,6 @@ import { fetchParseAndEnrichCodexLlms } from './c8y';
 
 export const useCodexContext = defineCachedFunction(async () => {
   return await fetchParseAndEnrichCodexLlms()
+}, {
+  maxAge: 60 * 60 * 24, // 24 hours
 })
